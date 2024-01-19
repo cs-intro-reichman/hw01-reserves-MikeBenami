7dd50cef132e7e27f6b28b7fe0e616d3fcf7cdd3
MikeBenami <146843399+MikeBenami@users.noreply.github.com>
2024-01-19 12:26:05 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,7 +1,7 @@
 public class Coins {
 	public static void main(String[] args) {
 		int m = Integer.parseInt(args[0]);
-		int q = m\25;
+		int q = m/25;
 		int c = m%25;
 		System.out.println("Use " + q + " quaters and " + c + " cents");
 	}
