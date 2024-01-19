e680f9c8e513de389e9ef6f1fdd209dfd804e892
MikeBenami <146843399+MikeBenami@users.noreply.github.com>
2024-01-19 13:18:19 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -3,6 +3,7 @@ public class Triangle {
 			int a = Integer.parseInt(args[0]);
 			int b = Integer.parseInt(args[1]);
 			int c = Integer.parseInt(args[2]);
-			System.out.println(a + ", " + b + ", " + c + ": " + boolean((a + b)> c));
+            boolean d = ((a + b)> c);
+			System.out.println(a + ", " + b + ", " + c + ": " + d );
 	}
 }
